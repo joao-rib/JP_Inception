8518a0b7b001e171ac25cd832f9acb6f9f731625
Joao Tiago Ribeiro Pereira <joao-rib@c2r1s6.42porto.com>
2025-09-08 13:52:38 +0100

    2025.09.08 - Learning wp stuff

---
+++ src/requirements/wordpress/conf/wp-config.php
@@ -0,0 +1,21 @@
+<!-- DEBIAN-ONLY -->
+
+<?php
+define( 'DB_NAME', getenv('WORDPRESS_DB_NAME') );
+define( 'DB_USER', getenv('WORDPRESS_DB_USER') );
+define( 'DB_HOST', getenv('WORDPRESS_DB_HOST') );
+define( 'DB_PASSWORD', trim( file_get_contents( getenv('WORDPRESS_DB_PASSWORD_FILE') ) ) );
+
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
+define( 'WP_DEBUG_DISPLAY', true );
+
+define( 'WP_HOME', 'https://' . getenv('DOMAIN_NAME') );
+define( 'WP_SITEURL', 'https://' . getenv('DOMAIN_NAME') );
+
+$table_prefix = 'wp_';
+
+if ( ! defined( 'ABSPATH' ) ) {
+    define( 'ABSPATH', __DIR__ . '/' );
+}
+require_once ABSPATH . 'wp-settings.php';
