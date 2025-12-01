1f203350dc336f3b716753554663e7343fbcf384
joao-rib <jonnasn@gmail.com>
2025-12-01 17:14:59 +0000

    2025.12.01 - MariaDB docker reviewed

--- src/requirements/mariadb/conf/init.sql
+++ src/requirements/mariadb/conf/init.sql
@@ -1,12 +1,15 @@
-### DEBIAN-ONLY
-
+--MariaDB installs with a root user without password. Attribute MYSQL_ROOT_PASSWORD to it;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+--Create remote root user if none exists;
 CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+--If root user exists, guarantee correct password;
 ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 
+--Create database;
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-
+--Create default user for the database;
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
 
+--Reload permissions and privileges;
 FLUSH PRIVILEGES;
--- src/requirements/mariadb/tools/start.sh
+++ src/requirements/mariadb/tools/start.sh
@@ -1,57 +1,24 @@
-#Language: !/bin/sh
+#!/bin/bash
 
 # If any command returns non-zero (error), then exit
 set -e
 
-# If MariaDB not yet installed, setup Maria DB (core install, user creation, etc.)
-# TODO (Alpine) installs MariaDB directly // mariadb-install-db --user=mysql --datadir=/var/lib/mysql
-# TODO (Debian) relies on a .sql script // envsubst < /etc/mysql/init.sql > /tmp/init.sql
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-	echo "Initialising MariaDB" # TODO Mensagem debug
+# If MariaDB not yet installed, setup Maria DB (core install, user creation)
+if [ -f /etc/mysql/init.sql ]; then
+	echo "Initialising MariaDB"
 
-    # TODO (Alpine) Saves directly in env variables
-	DB_ROOT_PASS=$(cat /run/secrets/dbrootpass)
-	WP_DB_PASS=$(cat /run/secrets/dbpass)
+	# Exports and interpolates into /tmp/init.sql
+	# If something fails, the env variables will not be exported
+	export MYSQL_ROOT_PASSWORD="$(cat /run/secrets/db_root_password)" && \
+	export MYSQL_PASSWORD="$(cat /run/secrets/db_password)" && \
+	envsubst < /etc/mysql/init.sql > /tmp/init.sql
+	# Defines mysql owner and permissions
+	chown mysql:mysql	/tmp/init.sql
+	chmod 660	/tmp/init.sql
 
-    # TODO (Debian) Exports and interpolates into /etc/mysql/init.sql
-    # export MYSQL_ROOT_PASSWORD="$(< /run/secrets/db_root_password)" && \
-    # export MYSQL_PASSWORD="$(< /run/secrets/db_password)" && \
-    # envsubst < /etc/mysql/init.sql > /tmp/init.sql
-
-	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
-	chown -R mysql:mysql /var/lib/mysql # TODO Examinar mais
-
-    # TODO - ALPINE
-    # Starts MariaDB temporarily with networking disabled.
-    # Waits for it to be ready using mysqladmin ping.
-    # Runs inline SQL to:
-    # Set root password.
-    # Create WP database.
-    # Create WP user with privileges.
-    # Shuts down that temporary server (kill "$pid").
-
-	su-exec mysql mariadbd \
-		--datadir=/var/lib/mysql \
-		--skip-networking --socket=/tmp/mysql.sock &
-	pid="$!"
-	
-	until mysqladmin ping --socket=/tmp/mysql.sock --silent; do
-		sleep 1
-	done
-
-	mysql -u root --socket=/tmp/mysql.sock <<-EOSQL
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';
-CREATE DATABASE IF NOT EXISTS \`$WP_DB_NAME\`;
-CREATE USER IF NOT EXISTS '$WP_DB_USER'@'%' IDENTIFIED BY '$WP_DB_PASS';
-GRANT ALL PRIVILEGES ON \`$WP_DB_NAME\`.* TO '$WP_DB_USER'@'%';
-FLUSH PRIVILEGES;
-EOSQL
-
-	kill "$pid"
+else
+	echo "No init.sql file found in /etc/mysql"
+	exit 1
 fi
 
-# (Alpine) Execute - TODO Examinar mais
-exec su-exec mysql mariadbd \
-	--datadir=/var/lib/mysql --bind-address=0.0.0.0
-
-# (Debian) - exec mariadbd
+exec mariadbd
