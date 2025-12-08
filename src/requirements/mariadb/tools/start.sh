1dca7672116dc1ad1d855f3b09fc4bc0727af5be
Joao Tiago Ribeiro Pereira <joao-rib@c1r3s2.42porto.com>
2025-12-08 18:31:44 +0000

    2025.12.08 - make

--- src/requirements/mariadb/tools/start.sh
+++ src/requirements/mariadb/tools/start.sh
@@ -9,7 +9,7 @@ if [ -f /etc/mysql/init.sql ]; then
 
 	# Exports and interpolates into /tmp/init.sql
 	# If something fails, the env variables will not be exported
-	export MYSQL_ROOT_PASSWORD="$(cat /run/secrets/db_root_password)" && \
+	export MYSQL_ROOT_PASSWORD="$(cat /run/secrets/dbroot_password)" && \
 	export MYSQL_PASSWORD="$(cat /run/secrets/db_password)" && \
 	envsubst < /etc/mysql/init.sql > /tmp/init.sql
 	# Defines mysql owner and permissions
