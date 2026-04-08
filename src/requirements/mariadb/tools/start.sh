781c7f734193489c929ba149e74afe39a6db7898
joao-rib <jonnasn@gmail.com>
2026-04-08 22:40:39 +0100

    2026.04.08 - Tamos na reta final

--- src/requirements/mariadb/tools/start.sh
+++ src/requirements/mariadb/tools/start.sh
@@ -13,7 +13,6 @@ if [ -f /etc/mysql/init.sql ]; then
 	export MYSQL_PASSWORD="$(cat /run/secrets/db_password)" && \
 	envsubst < /etc/mysql/init.sql > /var/lib/mysql/init.sql
 	# Defines mysql owner and permissions
-	# TODO WIP previously /var/lib/mysql/init.sql used to be /tmp/init.sql
 	chown mysql:mysql	/var/lib/mysql/init.sql
 	chmod 660	/var/lib/mysql/init.sql
 
@@ -23,4 +22,3 @@ else
 fi
 
 exec su -s /bin/bash mysql -c "mariadbd"
-# TODO old command was exec mariadbd
