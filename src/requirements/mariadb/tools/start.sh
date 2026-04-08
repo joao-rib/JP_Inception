f7512b6b27c5cac75e1367b543e65936bad9551d
joao-rib <jonnasn@gmail.com>
2026-04-08 21:52:45 +0100

    2026.04.08 - Fixing mariadb

--- src/requirements/mariadb/tools/start.sh
+++ src/requirements/mariadb/tools/start.sh
@@ -7,14 +7,15 @@ set -e
 if [ -f /etc/mysql/init.sql ]; then
 	echo "Initialising MariaDB"
 
-	# Exports and interpolates into /tmp/init.sql
+	# Exports and interpolates into /var/lib/mysql/init.sql
 	# If something fails, the env variables will not be exported
 	export MYSQL_ROOT_PASSWORD="$(cat /run/secrets/dbroot_password)" && \
 	export MYSQL_PASSWORD="$(cat /run/secrets/db_password)" && \
-	envsubst < /etc/mysql/init.sql > /tmp/init.sql
+	envsubst < /etc/mysql/init.sql > /var/lib/mysql/init.sql
 	# Defines mysql owner and permissions
-	chown mysql:mysql	/tmp/init.sql
-	chmod 660	/tmp/init.sql
+	# TODO WIP previously /var/lib/mysql/init.sql used to be /tmp/init.sql
+	chown mysql:mysql	/var/lib/mysql/init.sql
+	chmod 660	/var/lib/mysql/init.sql
 
 else
 	echo "No init.sql file found in /etc/mysql"
